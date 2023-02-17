96b38e37ab834a5298c0514cb6dbbc023ee2a8e6
harluss <26788867+harluss@users.noreply.github.com>
2023-02-17 13:58:09 +0000

    add shared test constant file

---
+++ space-hangar/src/test/java/com/project/hangar/common/Constant.java
@@ -0,0 +1,62 @@
+package com.project.hangar.common;
+
+import com.project.hangar.dto.SpaceshipDto;
+import com.project.hangar.dto.SpaceshipRequest;
+import com.project.hangar.dto.SpaceshipResponse;
+import com.project.hangar.entity.SpaceshipEntity;
+import lombok.experimental.UtilityClass;
+
+import java.util.UUID;
+
+@UtilityClass
+public class Constant {
+
+  public static final UUID ID = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+
+  static final String NAME = "Tie Fighter";
+
+  static final String CLASS_TYPE = "Starfighter";
+
+  static final int CREW = 1;
+
+  static final int PAYLOAD = 10;
+
+  public static SpaceshipDto buildDto() {
+    return SpaceshipDto.builder()
+        .id(ID)
+        .name(NAME)
+        .classType(CLASS_TYPE)
+        .crew(CREW)
+        .payload(PAYLOAD)
+        .build();
+  }
+
+  public static SpaceshipResponse buildResponse() {
+    return SpaceshipResponse.builder()
+        .id(ID)
+        .name(NAME)
+        .classType(CLASS_TYPE)
+        .crew(CREW)
+        .payload(PAYLOAD)
+        .build();
+  }
+
+  public static SpaceshipEntity buildEntity() {
+    return SpaceshipEntity.builder()
+        .id(ID)
+        .name(NAME)
+        .classType(CLASS_TYPE)
+        .crew(CREW)
+        .payload(PAYLOAD)
+        .build();
+  }
+
+  public static SpaceshipRequest buildRequest() {
+    return SpaceshipRequest.builder()
+        .name(NAME)
+        .classType(CLASS_TYPE)
+        .crew(CREW)
+        .payload(PAYLOAD)
+        .build();
+  }
+}
--- space-hangar/src/test/java/com/project/hangar/mapper/SpaceshipMapperImplTest.java
+++ space-hangar/src/test/java/com/project/hangar/mapper/SpaceshipMapperImplTest.java
@@ -7,21 +7,10 @@ import com.project.hangar.entity.SpaceshipEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
+import static com.project.hangar.common.Constant.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class SpaceshipMapperImplTest {
-
-  private static final UUID ID = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
-
-  private static final String NAME = "Tie Fighter";
-
-  private static final String CLASS_TYPE = "Starfighter";
-
-  private static final int CREW = 1;
-
-  private static final int PAYLOAD = 10;
 
   private SpaceshipMapperImpl spaceshipMapper;
 
@@ -32,9 +21,7 @@ class SpaceshipMapperImplTest {
 
   @Test
   void dtoToResponse() {
-    final SpaceshipDto dto = buildDtoNoId().toBuilder()
-        .id(ID)
-        .build();
+    final SpaceshipDto dto = buildDto();
     final SpaceshipResponse expected = buildResponse();
 
     final SpaceshipResponse actual = spaceshipMapper.dtoToResponse(dto);
@@ -45,7 +32,9 @@ class SpaceshipMapperImplTest {
   @Test
   void requestToDto() {
     final SpaceshipRequest request = buildRequest();
-    final SpaceshipDto expected = buildDtoNoId();
+    final SpaceshipDto expected = buildDto().toBuilder()
+        .id(null)
+        .build();
 
     final SpaceshipDto actual = spaceshipMapper.requestToDto(request);
 
@@ -54,12 +43,8 @@ class SpaceshipMapperImplTest {
 
   @Test
   void entityToDto() {
-    final SpaceshipEntity entity = buildEntityNoId().toBuilder()
-        .id(ID)
-        .build();
-    final SpaceshipDto expected = buildDtoNoId().toBuilder()
-        .id(ID)
-        .build();
+    final SpaceshipEntity entity = buildEntity();
+    final SpaceshipDto expected = buildDto();
 
     final SpaceshipDto actual = spaceshipMapper.entityToDto(entity);
 
@@ -68,8 +53,12 @@ class SpaceshipMapperImplTest {
 
   @Test
   void dtoToEntity() {
-    final SpaceshipDto dto = buildDtoNoId();
-    final SpaceshipEntity expected = buildEntityNoId();
+    final SpaceshipDto dto = buildDto().toBuilder()
+        .id(null)
+        .build();
+    final SpaceshipEntity expected = buildEntity().toBuilder()
+        .id(null)
+        .build();
 
     final SpaceshipEntity actual = spaceshipMapper.dtoToEntity(dto);
 
@@ -78,8 +67,11 @@ class SpaceshipMapperImplTest {
 
   @Test
   void updateEntityWithDto() {
-    final SpaceshipEntity entity = buildEntityNoId();
-    final SpaceshipDto updateDto = buildDtoNoId().toBuilder()
+    final SpaceshipEntity entity = buildEntity().toBuilder()
+        .id(null)
+        .build();
+    final SpaceshipDto updateDto = buildDto().toBuilder()
+        .id(null)
         .crew(5)
         .payload(100)
         .build();
@@ -90,42 +82,5 @@ class SpaceshipMapperImplTest {
     assertThat(entity.getClassType()).isEqualTo(updateDto.getClassType());
     assertThat(entity.getCrew()).isEqualTo(updateDto.getCrew());
     assertThat(entity.getPayload()).isEqualTo(updateDto.getPayload());
-  }
-
-  private SpaceshipDto buildDtoNoId() {
-    return SpaceshipDto.builder()
-        .name(NAME)
-        .classType(CLASS_TYPE)
-        .crew(CREW)
-        .payload(PAYLOAD)
-        .build();
-  }
-
-  private SpaceshipResponse buildResponse() {
-    return SpaceshipResponse.builder()
-        .id(ID)
-        .name(NAME)
-        .classType(CLASS_TYPE)
-        .crew(CREW)
-        .payload(PAYLOAD)
-        .build();
-  }
-
-  private SpaceshipEntity buildEntityNoId() {
-    return SpaceshipEntity.builder()
-        .name(NAME)
-        .classType(CLASS_TYPE)
-        .crew(CREW)
-        .payload(PAYLOAD)
-        .build();
-  }
-
-  private SpaceshipRequest buildRequest() {
-    return SpaceshipRequest.builder()
-        .name(NAME)
-        .classType(CLASS_TYPE)
-        .crew(CREW)
-        .payload(PAYLOAD)
-        .build();
   }
 }
