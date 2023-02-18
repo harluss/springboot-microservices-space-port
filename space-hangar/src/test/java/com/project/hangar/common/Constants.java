1c22070586d7c51ef5dca5273a6f9c7b1d5b38d4
harluss <26788867+harluss@users.noreply.github.com>
2023-02-18 12:40:01 +0000

    add controller tests

--- space-hangar/src/test/java/com/project/hangar/common/Constants.java
+++ space-hangar/src/test/java/com/project/hangar/common/Constants.java
@@ -4,22 +4,24 @@ import com.project.hangar.dto.SpaceshipDto;
 import com.project.hangar.dto.SpaceshipRequest;
 import com.project.hangar.dto.SpaceshipResponse;
 import com.project.hangar.entity.SpaceshipEntity;
+import com.project.hangar.exception.ErrorResponse;
 import lombok.experimental.UtilityClass;
+import org.springframework.http.HttpStatus;
 
 import java.util.UUID;
 
 @UtilityClass
 public class Constants {
 
-  public static final UUID ID = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+  private static final UUID ID = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
 
-  static final String NAME = "Tie Fighter";
+  private static final String NAME = "Tie Fighter";
 
-  static final String CLASS_TYPE = "Starfighter";
+  private static final String CLASS_TYPE = "Starfighter";
 
-  static final int CREW = 1;
+  private static final int CREW = 1;
 
-  static final int PAYLOAD = 10;
+  private static final int PAYLOAD = 10;
 
   public static SpaceshipDto buildDto() {
     return SpaceshipDto.builder()
@@ -62,5 +64,40 @@ public class Constants {
 
   public static UUID getRandomUUID() {
     return UUID.randomUUID();
+  }
+
+  public static SpaceshipRequest buildInvalidSpaceshipRequest() {
+    return SpaceshipRequest.builder()
+        .name(null)
+        .crew(-5)
+        .payload(20000)
+        .classType(" ")
+        .build();
+  }
+
+  public static ErrorResponse buildNotFoundErrorResponse() {
+    final HttpStatus status = HttpStatus.NOT_FOUND;
+
+    return ErrorResponse.builder()
+        .status(status.value())
+        .error(status.name())
+        .message("Spaceship not found")
+        .build();
+  }
+
+  public static ErrorResponse buildReqValidationFailedErrorResponse() {
+    final HttpStatus status = HttpStatus.BAD_REQUEST;
+    final ErrorResponse errorResponse = ErrorResponse.builder()
+        .status(status.value())
+        .error(status.name())
+        .message("Validation error. Check 'errors' field for details")
+        .build();
+
+    errorResponse.addValidationError("crew", "must be greater than or equal to 0");
+    errorResponse.addValidationError("payload", "must be less than or equal to 10000");
+    errorResponse.addValidationError("classType", "must not be blank");
+    errorResponse.addValidationError("name", "must not be blank");
+
+    return errorResponse;
   }
 }
---
+++ space-hangar/src/test/java/com/project/hangar/controller/SpaceshipControllerTest.java
@@ -0,0 +1,279 @@
+package com.project.hangar.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.hangar.dto.SpaceshipDto;
+import com.project.hangar.dto.SpaceshipRequest;
+import com.project.hangar.dto.SpaceshipResponse;
+import com.project.hangar.exception.ErrorResponse;
+import com.project.hangar.exception.NotFoundException;
+import com.project.hangar.mapper.SpaceshipMapper;
+import com.project.hangar.service.SpaceshipService;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static com.project.hangar.common.Constants.*;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.*;
+
+@WebMvcTest(SpaceshipController.class)
+class SpaceshipControllerTest {
+
+  private static final String TEST_API = "/api/spaceships";
+
+  private static final String TEST_API_WITH_ID = "/api/spaceships/{id}";
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private SpaceshipService spaceshipServiceMock;
+
+  @MockBean
+  private SpaceshipMapper spaceshipMapperMock;
+
+  private SpaceshipDto spaceshipDto;
+
+  private SpaceshipRequest spaceshipRequest;
+
+  private SpaceshipResponse spaceshipResponse;
+
+  @BeforeEach
+  void setUp() {
+    RestAssuredMockMvc.mockMvc(mockMvc);
+    spaceshipDto = buildDto();
+    spaceshipRequest = buildRequest();
+    spaceshipResponse = buildResponse();
+  }
+
+  @AfterEach
+  void tearDown() {
+    verifyNoMoreInteractions(spaceshipMapperMock, spaceshipServiceMock);
+  }
+
+  @Test
+  void getSpaceships() {
+    when(spaceshipServiceMock.getAll()).thenReturn(List.of(spaceshipDto));
+    when(spaceshipMapperMock.dtoToResponse(spaceshipDto)).thenReturn(spaceshipResponse);
+
+    given()
+        .when()
+        .get(TEST_API)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.OK.value())
+        .body(equalTo(toJsonString(List.of(spaceshipResponse))));
+
+    verify(spaceshipMapperMock).dtoToResponse(spaceshipDto);
+    verify(spaceshipServiceMock).getAll();
+  }
+
+  @Test
+  void getSpaceships_noShips() {
+    when(spaceshipServiceMock.getAll()).thenReturn(Collections.emptyList());
+
+    given()
+        .when()
+        .get(TEST_API)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.OK.value())
+        .body(equalTo(toJsonString(Collections.emptyList())));
+
+    verify(spaceshipServiceMock).getAll();
+    verifyNoInteractions(spaceshipMapperMock);
+  }
+
+  @Test
+  void getSpaceshipById() {
+    final UUID reqId = spaceshipResponse.getId();
+    when(spaceshipServiceMock.getById(reqId)).thenReturn(spaceshipDto);
+    when(spaceshipMapperMock.dtoToResponse(spaceshipDto)).thenReturn(spaceshipResponse);
+
+    given()
+        .when()
+        .get(TEST_API_WITH_ID, reqId)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.OK.value())
+        .body(equalTo(toJsonString(spaceshipResponse)));
+
+    verify(spaceshipMapperMock).dtoToResponse(spaceshipDto);
+    verify(spaceshipServiceMock).getById(reqId);
+  }
+
+  @Test
+  void getSpaceshipById_notFound() {
+    final UUID reqId = spaceshipResponse.getId();
+    final ErrorResponse expectedErrorResponse = buildNotFoundErrorResponse();
+    when(spaceshipServiceMock.getById(reqId)).thenThrow(new NotFoundException(expectedErrorResponse.getMessage()));
+
+    given()
+        .when()
+        .get(TEST_API_WITH_ID, reqId)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.NOT_FOUND.value())
+        .body(equalTo(toJsonString(expectedErrorResponse)));
+
+    verify(spaceshipServiceMock).getById(reqId);
+    verifyNoInteractions(spaceshipMapperMock);
+  }
+
+  @Test
+  void addSpaceship() {
+    when(spaceshipMapperMock.requestToDto(spaceshipRequest)).thenReturn(spaceshipDto);
+    when(spaceshipServiceMock.add(spaceshipDto)).thenReturn(spaceshipDto);
+    when(spaceshipMapperMock.dtoToResponse(spaceshipDto)).thenReturn(spaceshipResponse);
+
+    given()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(toJsonString(spaceshipRequest))
+        .when()
+        .post(TEST_API)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.OK.value())
+        .body(equalTo(toJsonString(spaceshipResponse)));
+
+    verify(spaceshipMapperMock).requestToDto(spaceshipRequest);
+    verify(spaceshipMapperMock).dtoToResponse(spaceshipDto);
+    verify(spaceshipServiceMock).add(spaceshipDto);
+  }
+
+  @Test
+  void addSpaceship_invalidRequestBody() {
+    final SpaceshipRequest invalidSpaceshipRequest = buildInvalidSpaceshipRequest();
+    final ErrorResponse expectedErrorResponse = buildReqValidationFailedErrorResponse();
+
+    final String responseBody = given()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(toJsonString(invalidSpaceshipRequest))
+        .when()
+        .post(TEST_API)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.BAD_REQUEST.value())
+        .extract()
+        .body()
+        .asString();
+
+    final ErrorResponse actualErrorResponse = toObject(responseBody, ErrorResponse.class);
+    assertThat(actualErrorResponse.getError()).isEqualTo(expectedErrorResponse.getError());
+    assertThat(actualErrorResponse.getStatus()).isEqualTo(expectedErrorResponse.getStatus());
+    assertThat(actualErrorResponse.getMessage()).isEqualTo(expectedErrorResponse.getMessage());
+    assertThat(actualErrorResponse.getErrors()).containsAll(expectedErrorResponse.getErrors());
+  }
+
+  @Test
+  void updateSpaceshipById() {
+    final UUID reqId = spaceshipResponse.getId();
+    when(spaceshipMapperMock.requestToDto(spaceshipRequest)).thenReturn(spaceshipDto);
+    when(spaceshipServiceMock.updateById(spaceshipDto, reqId)).thenReturn(spaceshipDto);
+    when(spaceshipMapperMock.dtoToResponse(spaceshipDto)).thenReturn(spaceshipResponse);
+
+    given()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(toJsonString(spaceshipRequest))
+        .when()
+        .put(TEST_API_WITH_ID, reqId)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.OK.value())
+        .body(equalTo(toJsonString(spaceshipResponse)));
+
+    verify(spaceshipMapperMock).requestToDto(spaceshipRequest);
+    verify(spaceshipMapperMock).dtoToResponse(spaceshipDto);
+    verify(spaceshipServiceMock).updateById(spaceshipDto, reqId);
+  }
+
+  @Test
+  void updateSpaceshipById_notFound() {
+    final UUID reqId = spaceshipResponse.getId();
+    final ErrorResponse expectedErrorResponse = buildNotFoundErrorResponse();
+    when(spaceshipMapperMock.requestToDto(spaceshipRequest)).thenReturn(spaceshipDto);
+    doThrow(new NotFoundException(expectedErrorResponse.getMessage()))
+        .when(spaceshipServiceMock).updateById(spaceshipDto, reqId);
+
+    given()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(toJsonString(spaceshipRequest))
+        .when()
+        .put(TEST_API_WITH_ID, reqId)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.NOT_FOUND.value())
+        .body(equalTo(toJsonString(expectedErrorResponse)));
+
+    verify(spaceshipMapperMock).requestToDto(spaceshipRequest);
+    verify(spaceshipServiceMock).updateById(spaceshipDto, reqId);
+  }
+
+  @Test
+  void deleteSpaceshipById() {
+    final UUID reqId = spaceshipResponse.getId();
+
+    given()
+        .when()
+        .delete(TEST_API_WITH_ID, reqId)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.NO_CONTENT.value());
+
+    verify(spaceshipServiceMock).deleteById(reqId);
+  }
+
+  @Test
+  void deleteSpaceshipById_notFound() {
+    final UUID reqId = spaceshipResponse.getId();
+    final ErrorResponse expectedErrorResponse = buildNotFoundErrorResponse();
+    doThrow(new NotFoundException(expectedErrorResponse.getMessage())).when(spaceshipServiceMock).deleteById(reqId);
+
+    given()
+        .when()
+        .delete(TEST_API_WITH_ID, reqId)
+        .then()
+        .log().body()
+        .assertThat()
+        .statusCode(HttpStatus.NOT_FOUND.value())
+        .body(equalTo(toJsonString(expectedErrorResponse)));
+
+    verify(spaceshipServiceMock).deleteById(reqId);
+  }
+
+  @SneakyThrows
+  private String toJsonString(final Object object) {
+    return objectMapper.writeValueAsString(object);
+  }
+
+  @SneakyThrows
+  private <T> T toObject(final String json, final Class<T> clazz) {
+    return objectMapper.readValue(json, clazz);
+  }
+}
