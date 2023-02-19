4eb10a3076117f88e9238c60b693761c582e81ac
harluss <26788867+harluss@users.noreply.github.com>
2023-02-19 13:54:50 +0000

    add exception handler and tests

---
+++ space-cantina/src/main/java/com/project/cantina/exception/ErrorResponse.java
@@ -0,0 +1,43 @@
+package com.project.cantina.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+  private Integer status;
+
+  private String error;
+
+  private String message;
+
+  private List<ValidationError> errors;
+
+  @Data
+  @Builder
+  private static class ValidationError {
+
+    private String field;
+
+    private String message;
+  }
+
+  public void addValidationError(final String field, final String message) {
+    if (Objects.isNull(errors)) {
+      errors = new ArrayList<>();
+    }
+
+    errors.add(ValidationError.builder()
+        .field(field)
+        .message(message)
+        .build());
+  }
+}
