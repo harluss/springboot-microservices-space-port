e8e83f14410cb4bbc978593f625fb774d3b4c921
harluss <26788867+harluss@users.noreply.github.com>
2023-02-19 10:16:26 +0000

    add cantina module, add cantina postgres container and sql script

---
+++ space-cantina/src/test/resources/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS crewmen
+(
+  id         uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  name       text NOT NULL,
+  species    text NOT NULL,
+  profession text NOT NULL,
+  weapons    text[]
+);
+
+INSERT INTO crewmen (name, species, profession, weapons)
+VALUES ('Han Solo', 'Human', 'Smuggler', ARRAY ['Blaster pistol']),
+       ('Chewbacca', 'Wookiee', 'Smuggler', ARRAY ['Bowcaster']),
+       ('Boba Fett', 'Human', 'Bounty hunter', ARRAY ['Blaster', 'Flame thrower']),
+       ('Mando', 'Human', 'Bounty hunter', ARRAY ['Blaster pistol']),
+       ('R2D2', 'Droid', 'Mechanic', ARRAY []::text[]),
+       ('Luke Skywalker', 'Human', 'Jedi Master', ARRAY ['Lightsaber']),
+       ('Greedo', 'Rodian', 'Bounty hunter', ARRAY ['Blaster pistol']),
+       ('IG-11', 'Driod', 'Assassin', ARRAY ['Blaster rifle']);
