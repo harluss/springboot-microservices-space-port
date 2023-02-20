b1c8c95486295e315ee0ac479d9a5eb67b64abf1
harluss <26788867+harluss@users.noreply.github.com>
2023-02-20 06:57:58 +0000

    update test schema files

--- space-cantina/src/test/resources/schema.sql
+++ space-cantina/src/test/resources/schema.sql
@@ -7,13 +7,15 @@ CREATE TABLE IF NOT EXISTS pilots
   weapons    text[]
 );
 
-INSERT INTO pilots (name, species, profession, weapons)
-VALUES ('Han Solo', 'Human', 'Smuggler', ARRAY ['DL-44 heavy blaster pistol']),
-       ('Chewbacca', 'Wookiee', 'Smuggler', ARRAY ['Bowcaster']),
-       ('Boba Fett', 'Human', 'Bounty hunter', ARRAY ['EE-3 blaster rifle', 'Z-X Flame-projector', 'Z-6 Jetpack']),
-       ('Mando', 'Human', 'Bounty hunter', ARRAY ['IB-94 blaster pistol', 'Beskar spear', 'Darksaber']),
-       ('R2D2', 'Droid', 'Mechanic', ARRAY []::text[]),
-       ('Luke Skywalker', 'Human', 'Jedi Master', ARRAY ['Lightsaber']),
-       ('Greedo', 'Rodian', 'Bounty hunter', ARRAY ['Blaster pistol']),
-       ('IG-11', 'Driod', 'Assassin',
+INSERT INTO pilots (id, name, species, profession, weapons)
+VALUES ('8ed6e335-56cb-4512-b1fb-5a55faa1057c', 'Han Solo', 'Human', 'Smuggler', ARRAY ['DL-44 heavy blaster pistol']),
+       ('e1d4e41b-c72e-4fb7-b3bd-6b86e96b20f1', 'Chewbacca', 'Wookiee', 'Smuggler', ARRAY ['Bowcaster']),
+       ('c77821c7-6b82-4d43-9837-2f7768610f45', 'Boba Fett', 'Human', 'Bounty hunter',
+        ARRAY ['EE-3 blaster rifle', 'Z-X Flame-projector', 'Z-6 Jetpack']),
+       ('84ced9d9-fb61-4eb0-a1c8-1c1ddf369ef1', 'Mando', 'Human', 'Bounty hunter',
+        ARRAY ['IB-94 blaster pistol', 'Beskar spear', 'Darksaber']),
+       ('1b6fc0c9-ab4c-4c01-98e3-7dac5395bdef', 'R2D2', 'Droid', 'Mechanic', ARRAY []::text[]),
+       ('e9ae8934-38ba-432b-a1c0-caa03ea73af9', 'Luke Skywalker', 'Human', 'Jedi Master', ARRAY ['Lightsaber']),
+       ('58ce4ada-ecac-454d-abfe-1a7ed8ae9ccf', 'Greedo', 'Rodian', 'Bounty hunter', ARRAY ['Blaster pistol']),
+       ('b91e6a4a-cfca-4fd3-8495-9701ea916abc', 'IG-11', 'Driod', 'Assassin',
         ARRAY ['E-11 medium blaster rifle', 'DLT-20A blaster rifle', 'Self-destruct mechanism']);
--- space-hangar/src/test/resources/schema.sql
+++ space-hangar/src/test/resources/schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS spaceships
   armament text[]
 );
 
-INSERT INTO spaceships (name, class, max_speed, payload, crew, crew_list, armament)
-VALUES ('Razor Crest', 'Gunship', 800, 70, 2, ARRAY []::uuid[], ARRAY ['Heavy laser cannons']),
-       ('Slave I', 'Starfighter', 1000, 40, 1, ARRAY []::uuid[], ARRAY ['Twin rotating blaster cannonTwin rotating blaster cannon', 'Proton torpedo tubes', 'Seismic charges']),
-       ('Millennium Falcon', 'Light freighter', 1200, 100, 2, ARRAY []::uuid[], ARRAY ['Quad laser cannons', 'Concussion missile tubes']),
-       ('X-wing', 'Starfighter', 1050, 10, 2, ARRAY []::uuid[], ARRAY ['Laser cannons', 'Proton torpedo launchers']);
+INSERT INTO spaceships (id, name, class, max_speed, payload, crew, crew_list, armament)
+VALUES ('d71649e9-c383-4e13-8d81-1f4e0145f3f0', 'Razor Crest', 'Gunship', 800, 70, 2, ARRAY ['84ced9d9-fb61-4eb0-a1c8-1c1ddf369ef1'::uuid, 'b91e6a4a-cfca-4fd3-8495-9701ea916abc'::uuid], ARRAY ['Heavy laser cannons']),
+       ('950126bc-1e4b-4792-a018-0d588660d499', 'Slave I', 'Starfighter', 1000, 40, 1, ARRAY ['c77821c7-6b82-4d43-9837-2f7768610f45'::uuid], ARRAY ['Twin rotating blaster cannonTwin rotating blaster cannon', 'Proton torpedo tubes', 'Seismic charges']),
+       ('9e075e25-3bd8-466e-b978-cf38a07ff85b', 'Millennium Falcon', 'Light freighter', 1200, 100, 2, ARRAY ['8ed6e335-56cb-4512-b1fb-5a55faa1057c'::uuid, 'e1d4e41b-c72e-4fb7-b3bd-6b86e96b20f1'::uuid], ARRAY ['Quad laser cannons', 'Concussion missile tubes']),
+       ('3fa85f64-5717-4562-b3fc-2c963f66afa6', 'X-wing', 'Starfighter', 1050, 10, 2, ARRAY ['e9ae8934-38ba-432b-a1c0-caa03ea73af9'::uuid, '1b6fc0c9-ab4c-4c01-98e3-7dac5395bdef'::uuid], ARRAY ['Laser cannons', 'Proton torpedo launchers']);
